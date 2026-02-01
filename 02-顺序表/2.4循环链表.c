714bbc90aba6d555637306c527145290deb2d305
linjinlong0717 <2269514780@qq.com>
2026-02-01 19:52:08 +0800

    新增循环链表

--- 02-顺序表/2.4循环链表.c
+++ 02-顺序表/2.4循环链表.c
@@ -1,82 +1,60 @@
-一、双链表的初始化(带头结点)
-//主要代码演示：
+一、循环单链表
+typedef struct LNode
+{
+	ElemType data;
+	struct LNode* next;
+}LNode,*LinkList;
+//初始化一个循环单链表
+bool InitList(LinkList* L)
+{
+	*L = (LNode*)malloc(sizeof(LNode));
+	if (*L == NULL) return false;
+	(*L)->next = *L;
+	return true;
+}
+//判断循环单链表是否为空
+bool Empty(LinkList L)
+{
+	if (L->next == L) return true;
+	else return false;
+}
+//技巧：很多时候对链表的操作都是在头部或尾部，
+可以让L指向表尾元素，这样时间复杂度就为O(1)
+
+二、循环双链表
 typedef struct DNode
 {
 	ElemType data;
 	struct DNode* prior, * next;
 }DNode,*DLinkList;
-//初始化双链表                
+//初始化双链表
 bool InitDLinkList(DLinkList* L)
 {
 	*L = (DNode*)malloc(sizeof(DNode));
-	if (*L == NULL)
-		return false;
-	(*L)->prior = NULL;
-	(*L)->next = NULL;
+	if (*L == NULL) return false;
+	(*L)->prior = *L;
+	(*L)->next = *L;
 	return true;
 }
 int main()
 {
-	//初始化双链表
 	DLinkList L;
 	InitDLinkList(&L);
-	//后续代码
+    //后续代码...
 }
 
-//判断双链表是否为空(带头结点)
-bool Empty(DLinkList L)
-{
-	if (L->next == NULL)
-		return true;
-	else
-		return false;
-}
-
-二、双链表的插入(在p结点之后插入s结点)
+//循环双链表的插入
 bool InsertNextDNode(DNode* p, DNode* s)
 {
 	if (p == NULL || s == NULL) return false;
 	s->next = p->next;
-	if (p->next != NULL) p->next->prior = s;
-	p->next = s;
+	p->next->prior = s;
 	s->prior = p;
+	p->next = s;
 	return true;
 }
 
-三、双链表的删除(删除p结点的后继结点)
-bool DeleteNextDNode(DNode* p)
-{
-	if (p == NULL) return false;
-	DNode* q = p->next;
-	if (q == NULL) return false;
-	p->next = q->next;
-	if (q->next != NULL)
-		q->next->prior = p;
-	free(q);
-	return true;
-}
-//那么删除这个双链表就可以：
-void DestroyList(DLinkList* L)
-{
-	while ((*L)->next != NULL)
-	{
-		DeleteNextDNode(*L);
-	}
-	free(*L);
-	*L = NULL;
-}
-
-四、双链表的遍历
-1.后向遍历
-while (p != NULL)
-{
-	//对结点p做相应处理
-	p = p->next; 
-}
-2.前向遍历
-while (p != NULL)  //若不处理头结点，则循环条件改成p->prior!=NULL
-{
-	//对结点p做相应处理
-	p = p->prior;
-}
-时间复杂度为：O(n)
+//循环双链表的删除操作(删除p的后继结点q)
+p->next = q->next;
+q->next->prior = p;
+free(q);
