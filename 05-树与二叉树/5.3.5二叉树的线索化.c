0752628a2320721745890a27f84a553428230847
linjinlong0717 <2269514780@qq.com>
2026-08-15 13:43:09 +0800

    新增二叉树的线索化

--- 05-树与二叉树/5.3.5二叉树的线索化.c
+++ 05-树与二叉树/5.3.5二叉树的线索化.c
@@ -1,25 +1,27 @@
 //二叉树线索化（Threaded Binary Tree）:
-把二叉树中那些闲着的空指针（NULL）利用起来，
+把二叉树中那些闲着的空指针（nullptr）利用起来，
 让它们指向遍历序列中的前驱或后继节点。
 
-一、中序线索化
-ThreadNode* pre = NULL;  //全局变量pre，指向当前访问的前驱
-typedef struct ThreadNode
+一、中序线索化(C++)
+ThreadNode* pre = nullptr;  //全局变量pre，指向当前访问的前驱
+struct ThreadNode
 {
-	ElemType data;
+	int data;
 	struct ThreadNode* lchild, * rchild;
 	bool ltag;  //false表示left指向左孩子，true表示left指向前驱线索
 	bool rtag;  //false表示right指向右孩子，true表示right指向后驱线索
 
+	ThreadNode(int val) : data(val), lchild(nullptr), rchild(nullptr),
+		ltag(0), rtag(0) {}
 };
 void visit(ThreadNode* q)
 {
-	if (q->lchild == NULL)
+	if (q->lchild == nullptr)
 	{
 		q->lchild = pre;
 		q->ltag = 1;
 	}
-	if (pre != NULL && pre->rchild == NULL)
+	if (pre != nullptr&& pre->rchild == nullptr)
 	{
 		pre->rchild = q;
 		pre->rtag = 1;
@@ -29,7 +31,7 @@ void visit(ThreadNode* q)
 typedef struct ThreadNode* ThreadTree;
 void InThread(ThreadTree T)
 {
-	if (T != NULL)
+	if (T != nullptr )
 	{
 		InThread(T->lchild);
 		visit(T);
@@ -38,28 +40,15 @@ void InThread(ThreadTree T)
 }
 void CreateInThread(ThreadTree T)
 {
-	pre = NULL;
-	if (T != NULL)
+	pre = nullptr;
+	if (T != nullptr)
 	{
 		InThread(T);
-		if (pre->rchild == NULL)
+		if (pre->rchild == nullptr)
 		{
 			pre->rtag = 1;//处理最后一个结点
 		}
 	}
 }
 //一边遍历，一边线索化
-
-二、后序线索化
-与中序线索化类似
-
-三、前序线索化
-void PreThread(ThreadTree T)
-{
-	if (T == NULL) return;
-	visit(T);
-	if (T->ltag == 0)     //注意：这里要判断是否为线索(否则会出现死循环)
-		PreThread(T->lchild);
-	if(T->rtag == 0)
-	    PreThread(T->rchild);
-}
+//一般是中序遍历的线索化
