1be1ac69a6b2e686fefa0c17e57fde2f78a32fa4
linjinlong0717 <2269514780@qq.com>
2026-08-23 16:46:20 +0800

    新增三路划分

---
+++ 08-排序/8.3.2.1快速排序优化-三路划分.c
@@ -0,0 +1,39 @@
+//三路划分
+void threewayPartition(vector<int>& v, int low, int high, int& l, int& r)
+{
+	l = low;
+	r = high;
+	int pivot = v[low];
+	int i = low + 1;
+	while (i <= r)
+	{
+		if (v[i] < pivot)
+		{
+			swap(v[i], v[l]);
+			i++;
+			l++;
+		}
+		else
+		{
+			if (v[i] > pivot)
+			{
+				swap(v[i], v[r]);
+				r--;
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+}
+void threewaySort(vector<int>& v, int low, int high)
+{
+	if (low < high)
+	{
+		int l, r;
+		threewayPartition(v, low, high, l, r);
+		threewaySort(v, low, l - 1);
+		threewaySort(v, r + 1, high);
+	}
+}
